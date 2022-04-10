80185d0847eb80df69900a2778692bc0f06f711c
tadzio8425 <js.ortegar1@uniandes.edu.co>
2022-04-10 01:39:14 -0500

    Sensor status second activity added

--- AppDev/App/RemotePet/app/src/main/java/com/example/remotepet/MainActivity.kt
+++ AppDev/App/RemotePet/app/src/main/java/com/example/remotepet/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.remotepet
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun openSensorActivity(view: View) : Void? {
+        val intent = Intent(this, SensorStatus::class.java)
+        startActivity(intent)
+        return null
+    }
 }
+
+
+
+
---
+++ AppDev/App/RemotePet/app/src/main/java/com/example/remotepet/SensorStatus.kt
@@ -0,0 +1,11 @@
+package com.example.remotepet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SensorStatus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sensor_status)
+    }
+}
